06ba64156a9264049cd56e1c7bc845f1b173c67e
AdrienMorvan <adrien.morvan@photobox.com>
2018-09-25 04:12:28 +0200

    Improved training quality with regulation

--- neuron_excitation.py
+++ neuron_excitation.py
@@ -1,9 +1,11 @@
 import torch
-from torch import nn
-from torchvision import models
+from torch import nn, Tensor
+from torchvision import models, transforms
+
+from image_utils.data_augmentation import crop
 from image_utils.vgg_normalizer import Normalization
 from image_utils.data_loading import save_image
-
+from nn_utils.losses import TVLoss, ImageNorm
 
 class NeuronExciter(torch.nn.Module):
 
@@ -13,28 +15,44 @@ class NeuronExciter(torch.nn.Module):
         self.neuron_index = neuron_index
         modules = list(nn_model.children())[:-1]
         self.pre_softmax = nn.Sequential(*modules)
-        self.loss = nn.L1Loss()
+
+        self.image_loss = ImageNorm()
+        self.tot_var = TVLoss()
+
+        self.lambda_tv = 0.001
+        self.lambda_norm = 0.01
 
     def forward(self, noise_image):
         # Get the right layer features for the content
-        noise_activation = self.pre_softmax.forward(noise_image).view(-1)
-        error = -noise_activation[self.neuron_index]
-        print("Error ", error, "activation", noise_activation[0])
-        return error
+        batch = noise_image.shape[0]
+        noise_activation = self.pre_softmax.forward(noise_image).view((batch, -1))
+        single_activity = torch.sum(torch.abs(noise_activation)) -\
+                          2 * torch.sum(torch.abs(noise_activation[self.neuron_index]))
+
+        error = (single_activity)
+        regularization = self.lambda_tv * self.tot_var(noise_image) + \
+            self.lambda_norm * self.image_loss(noise_image)
+        print("Error ", error, "reg ", regularization,
+              "activation", noise_activation[0][self.neuron_index],
+              "mean activation", torch.mean(noise_activation))
+        return error + regularization
 
 
 if __name__ == "__main__":
-    noise = torch.zeros((1, 3, 224, 224), requires_grad=True)
-    optim = torch.optim.LBFGS([noise.requires_grad_()], lr=0.1)
-    loss_estimator = NeuronExciter()
+    noise = torch.randn((1, 3, 250, 250), requires_grad=True)
+    #optim = torch.optim.LBFGS([noise.requires_grad_()])
+    optim = torch.optim.Adam([noise.requires_grad_()], lr=0.05)
+    loss_estimator = NeuronExciter(1)
 
     def closure():
         optim.zero_grad()
-        loss = loss_estimator.forward(noise)
+        jittered_batch = torch.stack([crop(noise.squeeze()) for _ in range(10)])
+        loss = loss_estimator.forward(jittered_batch)
         loss.backward()
         return loss
 
-    for i in range(200):
-        print(">>>", i)
-        save_image("./activ_image.jpg", noise)
+    for i in range(1500):
+        if i % 500 == 499:
+            print(">>>", i, torch.mean(noise))
+            save_image("./activ_image_{}.jpg".format(i), noise)
         optim.step(closure)
--- nn_utils/losses.py
+++ nn_utils/losses.py
@@ -4,12 +4,25 @@ import torch
 
 class TVLoss(nn.Module):
 
-    def __init__(self):
-        super(TVLoss, self).__init__()
-        self.filter_x =
+    def forward(self, tensor):
+
+        if len(tensor.size()) == 4:
+            pixel_dif1 = tensor[:, :, 1:, :] - tensor[:, :, :-1, :]
+            pixel_dif2 = tensor[:, :, :, 1:] - tensor[:, :, :, :-1]
+            tot_var = torch.sum(torch.abs(pixel_dif1)) + torch.sum(torch.abs(pixel_dif2))
+        elif len(tensor.size()) == 3:
+            pixel_dif1 = tensor[:, 1:, :] - tensor[:, :-1, :]
+            pixel_dif2 = tensor[:, :, 1:] - tensor[:, :, :-1]
+            tot_var = torch.sum(torch.abs(pixel_dif1)) + torch.sum(torch.abs(pixel_dif2))
+        else:
+            raise Exception("Tot var tensor should be 3D or 4D")
+
+        return tot_var
+
+
+class ImageNorm(nn.Module):
 
     def forward(self, tensor):
-        b, c, h, w = tensor.size()
-        z = c * h * w
-
+        return  torch.norm(tensor[tensor > 1] - 1, 2) + \
+                torch.norm(tensor[tensor < 0], 2)
 
