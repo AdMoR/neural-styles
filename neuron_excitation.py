85158350ca86861ef944c4777165c2f233ae3a93
admor <adrien_morvan@hotmail.fr>
2018-09-27 19:15:54 +0200

    Working visualisation git add neuron_excitation.py nn_utils/losses.pygit add neuron_excitation.py nn_utils/losses.pygit add neuron_excitation.py nn_utils/losses.py

--- neuron_excitation.py
+++ neuron_excitation.py
@@ -1,45 +1,40 @@
 import torch
 from torch import nn, Tensor
-from torchvision import models, transforms
+from torchvision import models
 
 from image_utils.data_augmentation import crop
 from image_utils.vgg_normalizer import Normalization
 from image_utils.data_loading import save_image
-from nn_utils.losses import TVLoss, ImageNorm
+from nn_utils.losses import TVLoss, ImageNorm, NeuronExcitationLoss, ExtremeSpikeLayerLoss
 
 class NeuronExciter(torch.nn.Module):
 
-    def __init__(self, neuron_index=0):
+    def __init__(self, layer_index=10, neuron_index=0, loss_type=ExtremeSpikeLayerLoss):
         super(NeuronExciter, self).__init__()
-        nn_model = models.resnet18(pretrained=True)
-        self.neuron_index = neuron_index
-        modules = list(nn_model.children())[:-1]
-        self.pre_softmax = nn.Sequential(*modules)
+        nn_model = models.alexnet(pretrained=True)
+        modules = list(nn_model.children())[:layer_index]
+        self.feature_layer = nn.Sequential(*modules)
 
         self.image_loss = ImageNorm()
         self.tot_var = TVLoss()
+        self.loss = loss_type(neuron_index)
 
         self.lambda_tv = 0.001
         self.lambda_norm = 0.01
 
     def forward(self, noise_image):
-        # Get the right layer features for the content
-        batch = noise_image.shape[0]
-        noise_activation = self.pre_softmax.forward(noise_image).view((batch, -1))
-        single_activity = torch.sum(torch.abs(noise_activation)) -\
-                          2 * torch.sum(torch.abs(noise_activation[self.neuron_index]))
+        # Get the right layer features
+        feature = self.feature_layer.forward(noise_image)
+        loss = self.loss(feature)
 
-        error = (single_activity)
         regularization = self.lambda_tv * self.tot_var(noise_image) + \
             self.lambda_norm * self.image_loss(noise_image)
-        print("Error ", error, "reg ", regularization,
-              "activation", noise_activation[0][self.neuron_index],
-              "mean activation", torch.mean(noise_activation))
-        return error + regularization
+
+        return loss + regularization
 
 
 if __name__ == "__main__":
-    noise = torch.randn((1, 3, 250, 250), requires_grad=True)
+    noise = torch.randn((1, 3, 224, 224), requires_grad=True)
     #optim = torch.optim.LBFGS([noise.requires_grad_()])
     optim = torch.optim.Adam([noise.requires_grad_()], lr=0.05)
     loss_estimator = NeuronExciter(1)
@@ -51,8 +46,8 @@ if __name__ == "__main__":
         loss.backward()
         return loss
 
-    for i in range(1500):
-        if i % 500 == 499:
+    for i in range(5000):
+        if i % 100 == 19:
             print(">>>", i, torch.mean(noise))
             save_image("./activ_image_{}.jpg".format(i), noise)
         optim.step(closure)
