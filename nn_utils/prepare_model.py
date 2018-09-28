6d327ef44d5ae53c01d8138559cd50f59d70afbb
admor <adrien_morvan@hotmail.fr>
2018-09-28 18:22:39 +0200

    Improved the organisation of the project

---
+++ nn_utils/prepare_model.py
@@ -0,0 +1,11 @@
+from torch import nn
+from torchvision import models
+
+from nn_utils.relu_override import replace_relu_with_leaky
+
+
+def load_alexnet(layer_index):
+    nn_model = models.alexnet(pretrained=True)
+    modules = list(nn_model.children())
+    replace_relu_with_leaky(modules[0])
+    return "alexnet_{}".format(layer_index), nn.Sequential(*modules[:layer_index])
