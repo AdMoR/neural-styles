08d04c49c954ca764deee509a5de2ec57916ac24
amor <adrien_morvan@hotmail.fr>
2018-10-13 17:28:44 +0200

    CUDA version with inception v3 handling

--- neuron_excitation.py
+++ neuron_excitation.py
@@ -7,11 +7,13 @@ from image_utils.data_loading import save_image
 from nn_utils.losses import TVLoss, ImageNorm, CenteredNeuronExcitationLoss, LayerExcitationLoss
 from nn_utils import prepare_model
 
+torch.set_default_tensor_type('torch.cuda.FloatTensor')
+
 class NeuronExciter(torch.nn.Module):
 
-    def __init__(self, layer_index=2, channel_index=5, loss_type=LayerExcitationLoss):
+    def __init__(self, layer_index=1, channel_index=6, loss_type=LayerExcitationLoss):
         super(NeuronExciter, self).__init__()
-        self.model_name, self.feature_layer = prepare_model.load_alexnet(layer_index)
+        self.model_name, self.feature_layer = prepare_model.load_inception_v3(layer_index)
 
         self.image_loss = ImageNorm()
         self.tot_var = TVLoss()
@@ -23,41 +25,42 @@ class NeuronExciter(torch.nn.Module):
     def forward(self, noise_image):
         # Get the right layer features
         feature = self.feature_layer.forward(noise_image)
-        print(feature.shape)
         loss = self.loss(feature)
-        print(loss)
 
         regularization = self.lambda_tv * self.tot_var(noise_image) + \
             self.lambda_norm * self.image_loss(noise_image)
 
         return loss + regularization
 
-
-if __name__ == "__main__":
+def optimize_image(layer_index=1, channel_index=6):
     noise = torch.randn((1, 3, 224, 224), requires_grad=True)
     #optim = torch.optim.LBFGS([noise.requires_grad_()])
     optim = torch.optim.Adam([noise.requires_grad_()], lr=0.05)
-    sigma = 10
-    loss_estimator = NeuronExciter()
+    sigma = 15
+    loss_estimator = NeuronExciter(layer_index, channel_index)
 
     def closure():
         optim.zero_grad()
         jittered_batch = torch.stack([jitter(noise.squeeze()) for _ in range(8)])
-        print(jittered_batch.shape)
         loss = loss_estimator.forward(jittered_batch)
         loss.backward()
         return loss
 
-    for i in range(2048):
-        if i % 100 == 19:
-            print(">>>", i, torch.mean(noise))
-            save_image("./images/{loss}_{model}_{channel}_{step}_{tv}.jpg".
+    n_steps = 2048
+    for i in range(n_steps):
+        optim.step(closure)
+        noise.numpy = gaussian_filter(noise.numpy, sigma=sigma)
+    save_image("./images/{loss}_{model}_{channel}_{step}_{tv}.jpg".
                        format(model=loss_estimator.model_name,
                               loss=loss_estimator.loss._get_name(),
                               channel=loss_estimator.loss.neuron_index,
                               tv=loss_estimator.lambda_tv,
-                              step=i),
-                       noise)
-            sigma -= 1
-        optim.step(closure)
-        noise.numpy = gaussian_filter(noise.numpy, sigma=sigma)
+                              step=n_steps),
+              noise)
+
+
+if __name__ == "__main__":
+    for i in range(10, 20):
+        optimize_image(layer_index=10, channel_index=i)
+        print("Finished on layer {}".format(i))
+
--- nn_utils/prepare_model.py
+++ nn_utils/prepare_model.py
@@ -10,3 +10,14 @@ def load_alexnet(layer_index):
     replace_relu_with_leaky(modules[0])
     print(modules)
     return "alexnet_{}".format(layer_index), nn.Sequential(*modules[:layer_index])
+
+
+def load_inception_v3(layer_index):
+    nn_model = models.inception_v3(pretrained=True)
+    modules = list(nn_model.children())
+    print(">>>>>>>>>")
+    replace_relu_with_leaky(modules)
+    print(modules)
+    return "inceptionv3_{}".format(layer_index), nn.Sequential(*modules[:layer_index])
+
+
