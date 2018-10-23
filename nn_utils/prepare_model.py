53a4391f5d643fa516f7037d3b8af07b7a4292a8
admor <adrien_morvan@hotmail.fr>
2018-10-23 21:48:15 +0200

    Added more nets and started custom grad

--- nn_utils/prepare_model.py
+++ nn_utils/prepare_model.py
@@ -1,7 +1,7 @@
 from torch import nn
 from torchvision import models
 
-from nn_utils.relu_override import replace_relu_with_leaky
+from nn_utils.relu_override import replace_relu_with_leaky, override_gradient_relu
 
 
 def load_alexnet(layer_index):
@@ -20,4 +20,30 @@ def load_inception_v3(layer_index):
     print(modules)
     return "inceptionv3_{}".format(layer_index), nn.Sequential(*modules[:layer_index])
 
+
+def load_resnet_18(layer_index):
+    nn_model = models.resnet18(pretrained=True)
+    modules = list(nn_model.children())
+    print(">>>>>>>>>")
+    override_gradient_relu(modules)
+    print(modules)
+    return "resnet18_{}".format(layer_index), nn.Sequential(*modules[:layer_index])
+
+
+def load_vgg_16(layer_index):
+    nn_model = models.vgg16(pretrained=True)
+    modules = list(nn_model.children())
+    print(">>>>>>>>>")
+    replace_relu_with_leaky(modules, ramp=0.01)
+    print(modules)
+    return "vgg16_{}".format(layer_index), nn.Sequential(*modules[:layer_index])
+
+
+def load_vgg_19(layer_index):
+    nn_model = models.vgg19(pretrained=True)
+    modules = list(nn_model.children())
+    print(">>>>>>>>>")
+    replace_relu_with_leaky(modules, ramp=0.01)
+    print(modules)
+    return "vgg19_{}".format(layer_index), nn.Sequential(*modules[:layer_index])
 
