b13fb5c35ae7959e9bef2e24298b5526071c4237
admor <adrien_morvan@hotmail.fr>
2018-11-03 14:18:38 +0100

    refactored img optimizer class

--- neuron_excitation.py
+++ neuron_excitation.py
@@ -1,86 +1,36 @@
 import torch
-from scipy.ndimage import gaussian_filter
-from scipy.ndimage.filters import median_filter
+from functools import partial
 
-from image_utils.data_augmentation import crop, jitter, build_subsampler, image_scaling, build_freq_img
-from image_utils.vgg_normalizer import Normalization
-from image_utils.data_loading import  save_optim
-from nn_utils.losses import TVLoss, ImageNorm, CenteredNeuronExcitationLoss, LayerExcitationLoss
+from image_utils.data_augmentation import jitter, build_freq_img
+from image_utils.data_loading import save_optim
 from nn_utils import prepare_model
+from nn_utils.losses import LayerExcitationLoss
+from optimizer_classes.visu_optimization import ParametrizedImageVisualizer
+
 
 torch.set_default_tensor_type('torch.cuda.FloatTensor')
 
-class NeuronExciter(torch.nn.Module):
 
-    def __init__(self, layer_index=1, channel_index=6, loss_type=LayerExcitationLoss):
-        super(NeuronExciter, self).__init__()
-        self.model_name, self.subsampler, self.feature_layer = prepare_model.load_vgg_16(layer_index)
+def run_optim(image_size=500, layer_index=33, lr=0.1, n_steps=2048):
+    model = prepare_model.load_alexnet(-1)
+    losses = [LayerExcitationLoss(layer_index)]
+    tfs = [partial(jitter, 16)]
 
-        self.image_loss = ImageNorm()
-        self.tot_var = TVLoss()
-        self.loss = loss_type(channel_index)
+    opt = ParametrizedImageVisualizer(losses=losses, model=model, transforms=tfs)
 
-        self.lambda_tv = 0.0015
-        self.lambda_norm = 10
+    noise = build_freq_img(image_size, image_size)
+    opt.run(noise, lr=lr, n_steps=n_steps)
 
-    def forward(self, noise_image):
-        # Get the right layer features
-        if noise_image.shape[-1] != 224:
-            noise_image = self.subsampler(noise_image)
-        feature = self.feature_layer.forward(noise_image)
-        loss = self.loss(feature)
-
-        regularization = self.lambda_tv * self.tot_var(noise_image) + \
-            self.lambda_norm * self.image_loss(noise_image)
-
-        print("loss : ", loss, "reg : ", regularization)
-
-        return loss + regularization
-
-def optimize_image(layer_index=10, channel_index=6, n_steps=2048, image_size=500, lr=0.05, debug=False):
-    #noise = build_freq_img(image_size, image_size)
-    noise = torch.randn((1, 3, image_size, image_size), requires_grad=True)
-    #optim = torch.optim.LBFGS([noise.requires_grad_()])
-    optim = torch.optim.Adam([noise.requires_grad_()], lr=lr)
-    sigma = 1
-    loss_estimator = NeuronExciter(layer_index, channel_index)
-
-    def closure():
-        optim.zero_grad()
-        
-        jittered_batch = torch.stack(
-            [jitter(noise.squeeze(), tau=8) 
-             for _ in range(16)]
-        )
-        #jittered_batch = image_scaling(jittered_batch, 1)
-        loss = loss_estimator.forward(jittered_batch)
-        loss.backward()
-        return loss
-
-    for i in range(n_steps):
-        #if i % 2048 == 1:
-        #    sigma /= 2
-        optim.step(closure)
-        #noise.numpy = gaussian_filter(noise.numpy, sigma=sigma)
-
-       
-        if False and i%100==1:
-            print(i)
-            save_optim(noise, model=loss_estimator.model_name,
-		       loss=loss_estimator.loss._get_name(),
-		       channel=loss_estimator.loss.neuron_index,
-		       tv=loss_estimator.lambda_tv,
-		       lr=lr,
-		       step=i)
-    save_optim(noise, model=loss_estimator.model_name,
-	loss=loss_estimator.loss._get_name(),
-	channel=loss_estimator.loss.neuron_index,
-	tv=loss_estimator.lambda_tv,
-	lr=lr, step=n_steps)
+    save_optim(noise, model=opt.model_name,
+               loss=opt.loss._get_name(),
+               channel=opt.loss.neuron_index,
+               tv=opt.lambda_tv,
+               lr=lr,
+               step=n_steps)
 
 
 if __name__ == "__main__":
     for i in range(40, 50):
-        optimize_image(channel_index=i, n_steps=2048, lr=0.08)
+        run_optim(layer_index=i, n_steps=2048, lr=0.08)
         print("Finished on channel {}".format(i))
 
---
+++ tests/test_param_visualizer.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+from functools import partial
+
+from image_utils.data_augmentation import jitter, build_freq_img
+from nn_utils import prepare_model
+from nn_utils.losses import LayerExcitationLoss
+from optimizer_classes.visu_optimization import ParametrizedImageVisualizer
+
+
+class TestParametrizedImageVisualizer(TestCase):
+
+    def test(self):
+        model = prepare_model.load_alexnet(-1)
+        losses = [LayerExcitationLoss(33)]
+        tfs = [partial(jitter, 16)]
+
+        opt = ParametrizedImageVisualizer(losses=losses, model=model, transforms=tfs)
+
+        noise = build_freq_img(224, 224)
+        opt.run(noise, lr=0.1, n_steps=2)
+
