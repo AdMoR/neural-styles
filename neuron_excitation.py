ec7517dcfc3f54517acdc3c4aba26892a525be6f
photoscience <photoscience@photoscience-gpu-test.frsar.pbx>
2018-12-26 19:45:41 +0100

    Chnaged implem of reg

--- neuron_excitation.py
+++ neuron_excitation.py
@@ -16,22 +16,23 @@ else:
     torch.set_default_tensor_type('torch.FloatTensor')
 
 
-def run_optim(image_size=500, layer_index=33, lr=0.005, n_steps=4096):
+def run_optim(image_size=224, layer_index=33, lr=0.005, n_steps=4096):
     model = prepare_model.load_vgg_16(0)
-    losses = [LayerExcitationLoss(layer_index, False), BatchDiversity(layer_index)]
+    losses = [LayerExcitationLoss(layer_index, False), BatchDiversity()]
     tfs = [partial(jitter, 4), scaled_rotation, partial(jitter, 16)]
 
-    opt = ParametrizedImageVisualizer(losses=losses, model=model, transforms=tfs, batch_size=4)
+    opt = ParametrizedImageVisualizer(losses=losses, model=model, transforms=tfs, batch_size=2)
 
-    #noise = build_freq_img(image_size, image_size)
-    noise = torch.randn((10, 3, image_size, image_size))
+    noise = torch.cat([build_freq_img(image_size, image_size)
+                       for _ in range(8)], dim=0)
+    #noise = torch.randn((8, 3, image_size, image_size))
     opt.run(noise, lr=lr, n_steps=n_steps)
 
     simple_save(noise, name=":".join([opt.name, str(n_steps), "{}"]))
 
 
 if __name__ == "__main__":
-    for i in range(0, 1000):
-        run_optim(layer_index=i, n_steps=4 * 1024, lr=0.1)
+    for i in range(1, 1000):
+        run_optim(layer_index=i, n_steps=2 * 1024, lr=0.03)
         print("Finished on channel {}".format(i))
 
