2ef15716d683cc78e78ce68d35a128d8ba866325
amor <adrien_morvan@hotmail.fr>
2022-07-11 21:58:22 +0200

    fix

--- svg_neuron_optim_with_style_reg.py
+++ svg_neuron_optim_with_style_reg.py
@@ -6,10 +6,7 @@ except:
     import diffvg as pydiffvg
 
 import urllib.request
-<<<<<<< HEAD
 import torch
-=======
->>>>>>> 190ef94d1f8ac5f0f4530611442d17e288f41417
 from neural_styles.svg_optim.excitation_forward_func import gen_vgg16_excitation_func_with_multi_style_regulation
 from neural_styles.svg_optim.svg_optimizer import CurveOptimizer, Generator
 from neural_styles.nn_utils.prepare_model import VGG16Layers
@@ -26,25 +23,19 @@ p = argparse.ArgumentParser()
 p.add_argument("--exc_layer_index", default=0, type=int)
 p.add_argument("--exc_layer_name", default=VGG16Layers.Conv5_3, type=VGG16Layers, choices=list(VGG16Layers))
 p.add_argument("--style_layer_name", default=VGG16Layers.Conv2_2, type=VGG16Layers, choices=list(VGG16Layers))
-<<<<<<< HEAD
 #p.add_argument("--img_path", required=True, type=str)
-=======
-p.add_argument("--img_path", default=None, type=str)
->>>>>>> 190ef94d1f8ac5f0f4530611442d17e288f41417
 p.add_argument("--n_paths", default=200, type=int)
 p.add_argument("--imsize", default=500, type=int)
 p.add_argument("--n_steps", default=1200, type=int)
 
 
 def run(n_paths, im_size, n_steps, exc_layer_name, exc_layer_index, style_layer_name, img_path):
-    link = "https://openprocessing.org/sketch/904490/thumbnail"
+    link = "https://openprocessing-usercontent.s3.amazonaws.com/thumbnails/visualThumbnail904490@2x.jpg"
     img_path = "local-filename.jpg"
     urllib.request.urlretrieve(link, img_path)
-<<<<<<< HEAD
-=======
 
     style_layer_names = [VGG16Layers.Conv1_2, VGG16Layers.Conv2_2]
->>>>>>> 190ef94d1f8ac5f0f4530611442d17e288f41417
+
 
     name = "result_" + "_".join([f"{k}{v}" for k, v in
                                  zip(["n_paths", "im_size", "n_steps", "exc_layer_name", "exc_layer_index",
@@ -53,7 +44,7 @@ def run(n_paths, im_size, n_steps, exc_layer_name, exc_layer_index, style_layer_
 
     with SummaryWriter(log_dir=f"./logs/{name}", comment=name) as writer:
         gen = Generator(n_paths, im_size, im_size)
-        func = gen_vgg16_excitation_func_with_multi_style_regulation(img_path, [style_layer_name], exc_layer_name,
+        func = gen_vgg16_excitation_func_with_multi_style_regulation(img_path, style_layer_names, exc_layer_name,
                                                                      exc_layer_index, 50.0, writer=writer)
         optimizer = CurveOptimizer(n_steps, im_size, im_size, gen.gen_func(), func)
         shapes, shape_groups = optimizer.gen_and_optimize(writer, color_optimisation_activated=False)
